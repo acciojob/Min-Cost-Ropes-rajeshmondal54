d0c979c52e31b0d57ae6d8b109f6c3cdd9367590
rajeshmondal54 <rm4223536@gmail.com>
2024-07-21 02:32:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function mincost(arr)
 { 
 //write your code here
+	if (arr.length <= 1) {
+        return 0;
+    }
+	const heap = new MinHeap(arr);
+
+    let totalCost = 0;
+	while (heap.length > 1) {
+		const first = heap.pop();
+        const second = heap.pop();
+		const cost = first + second;
+        totalCost += cost;
+		heap.push(cost);
+    }
+	return totalCost;
 // return the min cost
   
 }
